774ff36785cc528fb82d888fb394577d66380e60
Darcy <darcystud@gmail.com>
2024-10-07 13:09:49 +1100

    testing

---
+++ A2.java
@@ -0,0 +1,9 @@
+// Java program to illustrate Client side 
+// Implementation using DatagramSocket 
+
+public class A2 {
+    
+   
+    
+}
+
--- Matchmaking.java
+++ Matchmaking.java
@@ -6,20 +6,18 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 
-
 public class Matchmaking {
-    
 
     public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
-            System.out.println("Usage: java Program <broadcastAddress> <broadcastPort>");
+        if (args.length < 3) {
+            System.out.println("Usage: java Program <broadcastAddress> <broadcastPort> <server|client>");
             return;
         }
 
-        // Broadcast declarations
         InetAddress broadcastAddress = InetAddress.getByName(args[0]);
         int broadcastPort = Integer.parseInt(args[1]);
-        int tcpPort = broadcastPort + 1; // Use a different port for TCP connections
+        String role = args[2];
+        int tcpPort = broadcastPort + 1;
 
         DatagramSocket datagramSocket = new DatagramSocket();
         datagramSocket.setBroadcast(true);
@@ -30,56 +28,77 @@ public class Matchmaking {
         byte[] receiveData = new byte[1024];
         boolean gameFound = false;
 
-        while (!gameFound) {
-            System.out.println("Listening for [NEW GAME].....");
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            datagramSocket.setSoTimeout(30000);
-
-            try {
-                datagramSocket.receive(receivePacket);
-                String receivedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
-                System.out.println("Received message: " + receivedMessage);
-
-                if (receivedMessage.equals("NEW GAME")) {
-                    System.out.println("Match found. Connecting to player...");
-
-                    // Connect to the sender of the message via TCP (Player 1)
-                    InetAddress player1Address = receivePacket.getAddress();
-                    clientSocket = new Socket(player1Address, tcpPort);
-                    System.out.println("Connected to Player 1 via TCP: " + player1Address + " :" + tcpPort);
-
-                    gameFound = true;
-                }
-
-            } catch (SocketTimeoutException e) {
-                // No 'NEW GAME' message received within 30 seconds
-                System.out.println("No 'NEW GAME' message received within 30 seconds. Sending out [NEW GAME]...");
-
-                String newGameMessage = "NEW GAME";
-                byte[] sendData = newGameMessage.getBytes();
-                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcastAddress, broadcastPort);
-                datagramSocket.send(sendPacket);
-
-                System.out.println("[NEW GAME] message broadcasted. Waiting for others to connect...");
+        if (role.equals("server")) {
+            // Server logic
+            while (!gameFound) {
+                System.out.println("Listening for [NEW GAME].....");
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                datagramSocket.setSoTimeout(30000);
 
                 try {
-                    serverSocket = new ServerSocket(tcpPort); // Use tcpPort for the server
-                    serverSocket.setSoTimeout(30000);
+                    datagramSocket.receive(receivePacket);
+                    String receivedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                    System.out.println("Received message: " + receivedMessage);
 
-                    clientSocket = serverSocket.accept();
-                    System.out.println("Player connected: " + clientSocket.getInetAddress());
+                    if (receivedMessage.equals("NEW GAME")) {
+                        System.out.println("Match found. Connecting to player...");
 
-                    gameFound = true;
+                        InetAddress player1Address = receivePacket.getAddress();
+                        clientSocket = new Socket(player1Address, tcpPort);
+                        System.out.println("Connected to Player 1 via TCP: " + player1Address + " :" + tcpPort);
 
-                } catch (SocketTimeoutException ex) {
-                    System.out.println("No player connected within 30 seconds.");
-                    if (serverSocket != null && !serverSocket.isClosed()) {
-                        serverSocket.close();
+                        gameFound = true;
                     }
-                }
 
-            } catch (IOException e) {
-                e.printStackTrace();
+                } catch (SocketTimeoutException e) {
+                    System.out.println("No 'NEW GAME' message received within 30 seconds. Sending out [NEW GAME]...");
+
+                    String newGameMessage = "NEW GAME";
+                    byte[] sendData = newGameMessage.getBytes();
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcastAddress, broadcastPort);
+                    datagramSocket.send(sendPacket);
+
+                    System.out.println("[NEW GAME] message broadcasted. Waiting for others to connect...");
+
+                    try {
+                        serverSocket = new ServerSocket(tcpPort); 
+                        serverSocket.setSoTimeout(30000);
+
+                        clientSocket = serverSocket.accept();
+                        System.out.println("Player connected: " + clientSocket.getInetAddress());
+
+                        gameFound = true;
+
+                    } catch (SocketTimeoutException ex) {
+                        System.out.println("No player connected within 30 seconds.");
+                        if (serverSocket != null && !serverSocket.isClosed()) {
+                            serverSocket.close();
+                        }
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } else if (role.equals("client")) {
+            // Client logic
+            System.out.println("Sending [NEW GAME] to the network...");
+            String newGameMessage = "NEW GAME";
+            byte[] sendData = newGameMessage.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcastAddress, broadcastPort);
+            datagramSocket.send(sendPacket);
+
+            System.out.println("Waiting for server to accept the connection...");
+
+            try {
+                serverSocket = new ServerSocket(tcpPort);
+                serverSocket.setSoTimeout(30000);
+                clientSocket = serverSocket.accept();
+                System.out.println("Connected to server via TCP!");
+
+            } catch (SocketTimeoutException ex) {
+                System.out.println("Failed to connect to server within 30 seconds.");
             }
         }
 
@@ -96,7 +115,6 @@ public class Matchmaking {
             clientSocket.close();
         }
 
-        // Proceed with the game logic here
         System.out.println("Game setup complete. Ready to start the game.");
     }
 
